fd3ad406e82685dd3dc5fedb90906eb98072967d
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-22 00:46:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,7 +3,10 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
-
+birthday_kids =  {"Timmy" : "9", "Sarah" : "6", "Amanda" : "27"}
+birthday_kids [:Timmy]
+birthday_kids [:Sarah]
+birthday_kids [:Amanda]
 def happy_birthday(birthday_kids)
   # add your code snippet here!
 end
